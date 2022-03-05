861ba6747dd61fb27d696fdbc580bd1886d967c2
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-04 23:03:25 -0300

    remove transaction

--- script.js
+++ script.js
@@ -65,10 +65,11 @@ const DOM = {
     transactionsContainer: document.querySelector('#data-table tbody'),
     addTransaction(transaction, index) {
         const tr = document.createElement('tr')
-        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
         DOM.transactionsContainer.appendChild(tr)
     },
-    innerHTMLTransaction(transaction) {
+    innerHTMLTransaction(transaction, index) {
         const CSSclasses = transaction.amount > 0 ? "income" : "expense"
 
         const amount = Utils.formatCurrency(transaction.amount)
@@ -79,7 +80,7 @@ const DOM = {
                 <td class="${CSSclasses}">${amount}</td>
                 <td class="date">${transaction.date}</td>
                 <td>
-                    <img src="./assets/minus.svg" alt="Remover Transação">
+                    <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover Transação">
                 </td>
             
             `
@@ -184,8 +185,8 @@ const Form = {
 
 const App = {
     init() {
-        Transaction.all.forEach((transaction) => {
-            DOM.addTransaction(transaction)
+        Transaction.all.forEach((transaction, index) => {
+            DOM.addTransaction(transaction, index)
         })
         
         DOM.updateBalance()
