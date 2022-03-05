880bfb65a9dbd1c372d2db588fc6c516f3b785a4
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-05 00:12:39 -0300

    update tables using localStorage

--- script.js
+++ script.js
@@ -9,27 +9,18 @@ const Modal = {
     }
 }
 
+const Storage = {
+    get() {
+        return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+    },
+    set(transactions){
+        localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+    }
+}
 
 
 const Transaction = {
-    all: [
-        {
-            description: 'Luz',
-            amount: -50000,
-            date: '23/01/2021'
-        },
-        {
-            description: 'Website',
-            amount: 500000,
-            date: '23/01/2021'
-        },
-        {
-            description: 'Internet',
-            amount: -20000,
-            date: '23/01/2021'
-        },
-    
-    ],
+    all: Storage.get(),
     add(transaction){
         Transaction.all.push(transaction)
         App.reload()
@@ -124,9 +115,9 @@ const Utils = {
 }
 
 const Form = {
-    description: document.querySelector('#description'),
-    amount: document.querySelector('#amount'),
-    date: document.querySelector('#date'),
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
 
     getValues(){
         return {
@@ -183,13 +174,15 @@ const Form = {
     }
 }
 
+
+
 const App = {
     init() {
-        Transaction.all.forEach((transaction, index) => {
-            DOM.addTransaction(transaction, index)
-        })
+        Transaction.all.forEach(DOM.addTransaction)
         
         DOM.updateBalance()
+
+        Storage.set(Transaction.all)
     }, 
     reload() {
         DOM.clearTransactions()
@@ -199,9 +192,4 @@ const App = {
 
 App.init()
 
-Transaction.add({
-    description: 'Dieta',
-    amount: 70000,
-    date: '23/01/2021'
-})
 
